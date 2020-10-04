f0586850afcc7d25182b03f2ab1a30946cf92c8d
Shawn Williams <jr.william09@gmail.com>
2020-10-04 18:04:48 -0500

    adding styles using swiftUI for my contact card

--- ShawnCard/ContentView.swift
+++ ShawnCard/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.56, green: 0.27, blue: 0.68, opacity: 1.0)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("shawn")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0, alignment: .center)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Shawn James Williams Jr.")
+                    .font(Font.custom("Pacifico-Regular", size: 34))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS & Ionic Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+            }
+        }
     }
 }
 
